7a20fcc0aaf7b385a55a83d75c025ef744491ce3
Govindhareddysuma <118423596+Govindhareddysuma@users.noreply.github.com>
2023-02-13 13:54:48 +0530

    Array2.java

---
+++ Array2.java
@@ -0,0 +1,19 @@
+/*
+Write a java program using ArrayList ti print the following output:
+
+ArrayList: [Java, Python, Swift]
+*/
+
+
+import java.util.*;
+public class Array
+{
+    public static void main(String args[])
+    {
+        ArrayList al=new ArrayList();
+        al.add("Java");
+        al.add("Python");
+        al.add("Swift");
+        System.out.print("ArrayList: "+al);
+    }
+}
