567c1a2578db9784a04f67f6d4f46b11ce08b803
Govindhareddysuma <118423596+Govindhareddysuma@users.noreply.github.com>
2023-02-13 16:44:08 +0530

    Main2.java

---
+++ Main2.java
@@ -0,0 +1,24 @@
+/*
+Write a java program using the collection to display the following output:
+
+Methods: 
+
+To search an element in the stack, we use the search() method. It returns the position of the element from the top of the stack. 
+Sample Output: 
+Stack: [Dog, Horse, Cat]
+Position of Horse: 2
+*/
+
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Stack<String> stack=new Stack<>();
+        stack.push("Dog");
+        stack.push("Horse");
+        stack.push("Cat");
+        System.out.println("Stack: "+stack);
+        System.out.println("Position of Horse: "+stack.search("Horse"));
+    }
+}
